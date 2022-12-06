a9f220d96f0d5a09063483acf8697484954d8cd1
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-06 11:28:21 +0300

    simple Calculator
    simple Calculator by Python

---
+++ example2.py
@@ -0,0 +1,18 @@
+# Example 2 | simple Calculator via PYTHON
+#==========================================
+num1 = int (input ("Enter First Number: ")) 
+op = input ("Enter Operator: ")
+num2 = int (input ("Enter Second Number: "))
+if op == "+":
+    print (num1 + num2)
+if op == "-":
+    print (num1 - num2)
+if op == "*":
+    print (num1 * num2)
+if op == "/":
+    print (num1 / num2)
+#END---------------------------------------
+#==========================================
+# op = Operators = + - / * % ... etc. 
+# num = Number
+#==========================================
