389f1d3a92d653f661ea4e72a099067e60d0527c
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-07 13:25:25 +0300

    Emoji via PYTHON

---
+++ example3.py
@@ -0,0 +1,7 @@
+# Example 3 | Emoji via PYTHON
+#==========================================
+import emoji
+print(emoji.emojize('Python is :thumbs_up:'))
+print(emoji.emojize('what is :thumbs_up:'))
+input("Please press Enter to Exit !!!")
+#==========================================
