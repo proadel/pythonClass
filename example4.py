243016dd7ec1b2fe8fe72c85ae893742ab267810
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-07 14:17:32 +0300

    Dictionary by PYTHON

---
+++ example4.py
@@ -0,0 +1,18 @@
+# simple Dictionary by PYTHON
+#----------------------------
+# Start Header Of Dictionary
+print ("========================================")
+print ("    Simple.py | D I C T I O N A R Y ")
+print ("========================================")
+print ("  Developed By Adel Md. Umbrella Corp.")
+print ("  ver:0.1b | 2 English words to Arabic")
+print ("  word1 : run --- word2 : learn")
+print ("------------+967 733 477 848-------------")
+
+word = input ("Enter Your Eng.WORD : ")
+if word == "run":
+    print ("ركض - انطلق - دشن ")
+if word == "learn":
+    print ("يتعلم - يعرف - يتمرس")
+input ("press ENTER to Exit.")
+#===================================================
