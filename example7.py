983f694668578a5f1c7c85301566545bc045a342
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-08 20:18:36 +0300

    time Lib. and webbrowser
    # /search?q=

---
+++ example7.py
@@ -0,0 +1,12 @@
+# Time Lib. and Webbrowser
+# useing of |  urls.../search?q=  ... etc.
+#-------------------------------------------------------------
+import webbrowser
+import time
+webbrowser.open('https://www.google.com/search?q=coffee')
+time.sleep(10)
+webbrowser.open('http://www.google.com/search?q=rose')
+time.sleep(10)
+webbrowser.open('http://www.google.com/search?q=e-cars')
+#-------------------------------------------------------------
+
