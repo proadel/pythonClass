c85e9ad22338365281648c31ae823877c4500af1
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-08 19:50:41 +0300

    web-browser & PYTHON

---
+++ example6.py
@@ -0,0 +1,5 @@
+# how use webbrowser lib to send you up to url
+#----------------------------------------------
+import webbrowser
+webbrowser.open('https://www.yahoo.com/')
+#----------------------------------------------
