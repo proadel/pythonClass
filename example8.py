eaf4dbab43345f55f8acdf42af706f47e4d4706a
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-08 20:48:18 +0300

    webbrowser |  google/Search TOOL

---
+++ example8.py
@@ -0,0 +1,14 @@
+# webbrowser | eg: google/Search TOOL
+#===============================================================
+import webbrowser
+print ('===============================')
+print (' Google Search TOOL "HOME-MADE"')
+print (' ====DEVELOPED BY ADEL MD.====')
+search = input ('enter your SEARCH:')
+webbrowser.open('https://www.google.com/search?q= ' + search)
+print ('===============================')
+print (' Free-contact +967 733 744 848')
+print (' adeldawood@live.com | umbrella')
+print ('===============================')
+input ('press ENTER to EXIT')
+#---------------------------------------------------------------
