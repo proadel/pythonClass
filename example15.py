37a21a0eaa7d9b8a9c3f49e0745985d5b432ea36
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-10 00:49:08 +0300

    CREATE TABLE mysql.py

---
+++ example15.py
@@ -0,0 +1,33 @@
+# CREATE TABLE mysql.py
+# DATABASE mysql.py
+# SQL codes : CREATE TABLE
+#----------------------------------------------------
+import mysql.connector
+try:
+    conn = mysql.connector.connect(
+        host = 'localhost' ,
+        user = 'root' ,
+        passwd = '' ,
+        database = 'db1py' ,
+        )      
+    mycur = conn.cursor()
+    mycur.excute("CREATE TABLE tablePy1 (tablePy1id int primary key , tablePy1name varchar(50))")
+except mysql.connector.Error as r:
+    print (r)
+#----------------------------------------------------
+#---B-A-S-I-C---C-O-D-E--------------------------
+'''
+import mysql.connector
+try:
+    conn = mysql.connector.connect(
+        host = 'localhost' ,
+        user = 'root' ,
+        passwd = '' ,
+        database = '' ,
+        )
+    mycur = conn.cursor()
+    mycur.excute()
+except mysql.connector.Error as r:
+    print (r)
+'''
+#------------------------------------------------
