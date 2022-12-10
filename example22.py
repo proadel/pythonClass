cecd07a1101ab01766985849251618f04e7a9069
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-10 19:13:15 +0300

    File.py to py.exe and change icon

---
+++ example22.py
@@ -0,0 +1,15 @@
+# convert file.py to py.exe and change icone
+#-------------------------------------------
+# python installer lib.
+#C:\Users\..>pip install pyinstaller
+# to make py.exe files
+#C:\Users\urPC.path\..>pyinstaller --onefile your_filename
+#-->press Enter
+# to change file icon = filename.ico
+#convert img.png xxx.svg ... to ...ico
+#C:\Users\urPC.path\..>pyinstaller --onefile --icone='name_img.ico' your_filename
+#-->press Enter
+#-------------------------------------------
+#print ('Good, Your file now Run as exe')
+#input ('Please press ENTER to EXIT')
+#-------------------------------------------
