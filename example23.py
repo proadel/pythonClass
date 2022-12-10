d33b91cbe9b91f9ac2731d3ae4ab88f1d8d67a16
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-10 19:48:18 +0300

    GUI creation by tkinter Lib.

---
+++ example23.py
@@ -0,0 +1,10 @@
+# GUI creation by tkinter Lib.
+#----------------------------------
+from tkinter import *
+root = Tk()
+root.mainloop()
+#------------------------------------------------------------
+# to change file to exe and to remove
+# also to remove black screen (black window) add --windowed
+#c:\\yourpat>pyintaller --onefile --windowed yourFile_name
+#------------------------------------------------------------
